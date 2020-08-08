b85f4a1efd5e2347231423e835b912fdfc4791de
Kunal Kak <kunalrkak@gmail.com>
2020-08-07 22:33:10 -0400

    Close site display notice

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,19 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, only: [:guidelines]
+  before_action :authenticate_user!, only: [:guidelines, :notice]
 
   def index
     if user_signed_in?
-      redirect_to reservations_url
+      # TODO: REMOVE NOTICE
+      # redirect_to reservations_url
+      redirect_to home_notice_url
     else 
       redirect_to new_user_session_url
     end
   end
 
   def guidelines
+  end
+
+  def notice
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'reservations/me', to: 'reservations#me'
-  resources :reservations, only: [:index, :show, :create, :destroy]
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  # TODO: REMOVE NOTICE
+  # get 'reservations/me', to: 'reservations#me'
+  # resources :reservations, only: [:index, :show, :create, :destroy]
+  #   authenticate :user, lambda { |u| u.admin? } do
+  #     mount Sidekiq::Web => '/sidekiq'
+  #   end
 
   devise_for :users, :controllers => { registrations: 'users/registrations' }
-  get 'users/all', to: 'users#all'
-  get 'home/guidelines', to: 'home#guidelines'
+  # get 'users/all', to: 'users#all'
+  # get 'home/guidelines', to: 'home#guidelines'
+  get 'home/notice', to: 'home#notice'
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
